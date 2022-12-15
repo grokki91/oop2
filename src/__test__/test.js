1b32ac9d3b87b86163ca4c38f34dc3ed953394d5
Pavel Chekalin <grokki91@gmail.com>
2022-12-16 01:29:06 +0300

    Add tests

--- src/__test__/test.js
+++ src/__test__/test.js
@@ -1,6 +1,27 @@
-import Bowerman from '../app/Bowerman';
+import Character from '../app/Character';
 
-test('Check min length name in class Character', () => {
-  const hero = new Bowerman('Nana', 'Bowman');
-  expect(hero).not.toThrow();
+describe('Check field name in class Character', () => {
+  test('Check empty name in class Character', () => {
+    expect(() => new Character('', 'Bowman')).toThrow();
+  });
+
+  test('Check long name in class Character', () => {
+    expect(() => new Character('Super long name', 'Bowman')).toThrow();
+  });
+
+  test('Check correct name in class Character', () => {
+    const expextedHero = {
+      health: 100,
+      level: 1,
+      name: 'Monster',
+      type: 'Bowman',
+    };
+    expect(new Character('Monster', 'Bowman')).toEqual(expextedHero);
+  });
+});
+
+describe('Check field type in class Character', () => {
+  test('Check wrong value in type', () => {
+    expect(() => new Character('Monster', '')).toThrow();
+  });
 });
